883c3c9da82b1f6296c10c8dc04f4cd3f87ba2a8
Bidló Balázs Ádám <bidlo.balazs@simonyi.bme.hu>
2022-10-14 15:08:42 +0200

    checkin size

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -216,7 +216,7 @@ export class HomeComponent implements OnInit {
 
   openCheckinDialog() {
     this.dialog.open(CheckinDialog, {
-      width: '90%',
+      width: '100%',
       maxWidth: '800px',
       data: {tickets: this.ticketDataSource.data},
     });
